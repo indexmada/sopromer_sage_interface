36dfee2710d4cb4a117472aadec8436504b695a6
Eric <ramaroson.profile@gmail.com>
2024-11-23 09:46:24 +0300

    add file d attente

--- models/file_queue.py
+++ models/file_queue.py
@@ -5,8 +5,9 @@ class FileImportQueue(models.Model):
     _name = 'file.import.queue'
     _description = 'Queue pour gérer l\'importation des fichiers'
 
-    name = fields.Char(string='Nom du fichier', required=True)
-    reference = fields.Char(string='Référence du fichier', required=True, unique=True)
+    name = fields.Char(string='Emplacement du fichier', required=True)
+    reference = fields.Char(string='Nom du fichier', required=True)
+    reference_stock = fields.Char(string='Référence Stock', required=True, unique=True)
     status = fields.Selection([
         ('pending', 'En attente'),
         ('processing', 'En cours'),
@@ -19,12 +20,14 @@ class FileImportQueue(models.Model):
         with open(file_path, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                # Supposons que le 'name' du stock.picking se trouve dans une colonne 'picking_name' du CSV
-                picking_name = row.get('picking_name')  # Remplacer 'picking_name' par le nom de colonne réel dans le CSV
+                # Supposons que la 5e colonne soit 'name'
+                reference_value = row.get('name')  # Utiliser la colonne 'name' du fichier CSV comme référence
 
                 # Créer un enregistrement dans la file d'attente avec la référence du stock.picking
                 self.env['file.import.queue'].create({
                     'name': row.get('name'),  # Nom du fichier (ou autre champ qui peut identifier le fichier)
-                    'reference': picking_name,  # La référence correspond au 'name' du stock.picking
+                    'reference_stock': reference_value,  # La référence est maintenant la 5e colonne (name)
                     'status': 'pending',  # Statut initial
                 })
+
+
