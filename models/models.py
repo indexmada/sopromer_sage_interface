846bad04bb7510e61b6801a65bd717ab0824be81
Eric <ramaroson.profile@gmail.com>
2024-12-02 23:36:10 +0300

    reajuste methode import stock

--- models/models.py
+++ models/models.py
@@ -33,7 +33,7 @@ class FileImportQueue(models.Model):
 	_description = 'Queue pour gérer l\'importation des fichiers'
 
 	name = fields.Char(string='Nom du fichier', required=True)
-	reference = fields.Char(string='Référence du fichier', required=True, unique=True)
+	stock_reference = fields.Char(string='Référence du fichier', required=True, unique=True)
 	status = fields.Selection([
 	    ('pending', 'En attente'),
 	    ('processing', 'En cours'),
